fc0bb66e251de2dd5b0877c7c5200be8f01cbcb8
amikove <amikove@gmail.com>
2026-06-21 18:33:05 +0100

    Remove seed from startup

--- start.js
+++ start.js
@@ -1,10 +1,1 @@
-const { execSync } = require('child_process')
-
-try {
-  console.log('🌱 Lancement du seed...')
-  execSync('node src/db/seed.js', { stdio: 'inherit' })
-} catch (e) {
-  console.log('Seed déjà fait ou erreur ignorée:', e.message)
-}
-
 require('./src/index.js')
