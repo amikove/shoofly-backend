ee7e64f0e288fcce09e9e29a4381fe30aee0ab93
amikove <amikove@gmail.com>
2026-06-21 18:53:51 +0100

    Restore seed with check

--- start.js
+++ start.js
@@ -1,1 +1,8 @@
+
+const { execSync } = require('child_process')
+try {
+  execSync('node src/db/seed.js', { stdio: 'inherit' })
+} catch (e) {
+  console.log('Seed ignoré:', e.message)
+}
 require('./src/index.js')
